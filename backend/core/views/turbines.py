0e694cf4fbe9cc7e8ed5c37a51526aeb87f6fa71
Tatabbyi <ytrachel0@gmail.com>
2026-01-07 13:51:22 +0000

    fix: Pagination

--- backend/core/views/turbines.py
+++ backend/core/views/turbines.py
@@ -6,7 +6,7 @@ from core.serializers.turbines import TurbineSerializer
 
 
 class TurbinePagination(PageNumberPagination):
-    page_size = 50
+    page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
